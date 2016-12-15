4488a0bf052a9fe0335bed99cf19f4bc9de993df
Sali Zumberi <sali.zumberi@live.de>
2016-12-15 10:54:14 +0100

    cleanup

--- src/main/java/dockalyzer/services/DatabaseService.java
+++ src/main/java/dockalyzer/services/DatabaseService.java
@@ -1,7 +1,5 @@
 package dockalyzer.services;
 
-import dockalyzer.app.Student;
-import dockalyzer.models.Frage1;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,9 +7,6 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
-import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -42,168 +37,5 @@ public class DatabaseService {
 
         // Create the SessionFactory using the ServiceRegistry
         SESSION_FACTORY = config.buildSessionFactory(registry);
-    }
-
-    public static void addData(Date docker, Date project, Date difference) {
-        // Create a session
-        Session session = SESSION_FACTORY.openSession();
-        Transaction transaction = null;
-        try {
-            // Begin a transaction
-            transaction = session.beginTransaction();
-            Frage1 stu = new Frage1();
-            //stu.setId(id);
-            stu.setProjectDate(project);
-            stu.setDockerfileDate(docker);
-            stu.setDockerfileDate(difference);
-            // Save the student
-            session.save(stu);
-            // Commit the transaction
-            transaction.commit();
-        } catch (HibernateException ex) {
-            // If there are any exceptions, roll back the changes
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            // Print the Exception
-            ex.printStackTrace();
-        } finally {
-            // Close the session
-            session.close();
-        }
-    }
-
-    /**
-     * Create a new Student.
-     *
-     * @param name
-     * @param age
-     */
-    public static void create(String name, int age) {
-        // Create a session
-        Session session = SESSION_FACTORY.openSession();
-        Transaction transaction = null;
-        try {
-            // Begin a transaction
-            transaction = session.beginTransaction();
-            Student stu = new Student();
-            //stu.setId(id);
-            stu.setName(name);
-            stu.setAge(age);
-            // Save the student
-            session.save(stu);
-            // Commit the transaction
-            transaction.commit();
-        } catch (HibernateException ex) {
-            // If there are any exceptions, roll back the changes
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            // Print the Exception
-            ex.printStackTrace();
-        } finally {
-            // Close the session
-            session.close();
-        }
-    }
-
-    /**
-     * Read all the Students.
-     *
-     * @return a List of Students
-     */
-    public static List<Student> readAll() {
-        List<Student> students = null;
-        // Create a session
-        Session session = SESSION_FACTORY.openSession();
-        Transaction transaction = null;
-        try {
-            // Begin a transaction
-            transaction = session.beginTransaction();
-            students = session.createQuery("FROM Student").list();
-            // Commit the transaction
-            transaction.commit();
-        } catch (HibernateException ex) {
-            // If there are any exceptions, roll back the changes
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            // Print the Exception
-            ex.printStackTrace();
-        } finally {
-            // Close the session
-            session.close();
-        }
-        return students;
-    }
-
-    /**
-     * Delete the existing Student.
-     *
-     * @param id
-     */
-    public static void delete(int id) {
-        // Create a session
-        Session session = SESSION_FACTORY.openSession();
-        Transaction transaction = null;
-        try {
-            // Begin a transaction
-            transaction = session.beginTransaction();
-            // Get the Student from the database.
-            Student stu = (Student) session.get(Student.class,
-                    Integer.valueOf(id));
-            // Delete the student
-            session.delete(stu);
-            // Commit the transaction
-            transaction.commit();
-        } catch (HibernateException ex) {
-            // If there are any exceptions, roll back the changes
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            // Print the Exception
-            ex.printStackTrace();
-        } finally {
-            // Close the session
-            session.close();
-        }
-    }
-
-    /**
-     * Update the existing Student.
-     *
-     * @param id
-     * @param name
-     * @param age
-     */
-    public static void upate(int id, String name, int age) {
-        // Create a session
-        Session session = SESSION_FACTORY.openSession();
-        Transaction transaction = null;
-        try {
-            // Begin a transaction
-            transaction = session.beginTransaction();
-            // Get the Student from the database.
-            Student stu = (Student) session.get(Student.class,
-                    Integer.valueOf(id));
-            // Change the values
-            stu.setName(name);
-            stu.setAge(age);
-            // Update the student
-            session.update(stu);
-
-            // Commit the transaction
-            transaction.commit();
-        } catch (HibernateException ex) {
-            // If there are any exceptions, roll back the changes
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            // Print the Exception
-            ex.printStackTrace();
-        } finally {
-            // Close the session
-            session.close();
-        }
     }
 }
