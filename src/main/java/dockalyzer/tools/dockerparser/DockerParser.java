194f103f64c3e5de912775db94ad152cedb505eb
Sali Zumberi <sali.zumberi@live.de>
2016-12-15 10:52:50 +0100

    fixxes on DockerParser

--- src/main/java/dockalyzer/tools/dockerparser/DockerParser.java
+++ src/main/java/dockalyzer/tools/dockerparser/DockerParser.java
@@ -6,8 +6,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import dockalyzer.models.DockerFile;
+import dockalyzer.models.DockerfileSnapshot;
+import dockalyzer.models.SQL.Snapshot;
 import dockalyzer.models.commands.*;
-import jdk.nashorn.internal.ir.Labels;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonMethod;
@@ -27,26 +28,28 @@ import java.util.stream.Stream;
  * Created by salizumberi-laptop on 22.10.2016.
  */
 public class DockerParser {
-    DockerFile dockerfile;
+    Snapshot dockerfile = new Snapshot();
     public String localDockerfilePath;
     public String localPath;
-    public List<From> froms = new ArrayList<>();
-    public List<Maintainer> maintainers = new ArrayList<>();
+
+    public From from;
+    public Maintainer maintainer;
+    public Cmd cmd;
+    public EntryPoint entryPoint;
+    public StopSignal stopSignal;
+    public Healthcheck healthcheck;
+
     public List<Run> runs = new ArrayList<>();
-    public List<Cmd> cmds = new ArrayList<>();
     public List<Label> labels = new ArrayList<>();
     public List<Env> envs = new ArrayList<>();
     public List<Expose> exposes = new ArrayList<>();
     public List<Add> adds = new ArrayList<>();
     public List<Copy> copies = new ArrayList<>();
-    public List<EntryPoint> entryPoints = new ArrayList<>();
     public List<Volume> volumes = new ArrayList<>();
     public List<User> users = new ArrayList<>();
     public List<WorkDir> workDirs = new ArrayList<>();
     public List<Arg> args = new ArrayList<>();
     public List<OnBuild> onBuilds = new ArrayList<>();
-    public List<StopSignal> stopSignals = new ArrayList<>();
-    public List<Healthcheck> healthchecks = new ArrayList<>();
 
     public DockerParser(String localpath, String localDockerfilePath) {
         String[] pathTokens = localDockerfilePath.split("/");
@@ -62,9 +65,22 @@ public class DockerParser {
         this.localPath = localpath;
     }
 
-    public DockerFile getDockerfileObject() throws IOException {
-        dockerfile = new DockerFile();
-        File flatDockerfile = getFlatDockerFile();
+    public DockerParser(String localpath) {
+        this.localDockerfilePath = "";
+        this.localPath = localpath;
+    }
+
+    public Snapshot getDockerfileObject(String fileName) throws IOException {
+        dockerfile = new Snapshot();
+        File flatDockerfile = getFlatDockerFile(new File(fileName));
+        doClassificationOfLines(flatDockerfile);
+        assignToDockerObject();
+        return dockerfile;
+    }
+
+    public Snapshot getDockerfileObject() throws IOException {
+        dockerfile = new Snapshot();
+        File flatDockerfile = getFlatDockerFile(new File(this.localPath + "/" + this.localDockerfilePath + "/" + "Dockerfile"));
         doClassificationOfLines(flatDockerfile);
         assignToDockerObject();
         return dockerfile;
@@ -77,8 +93,10 @@ public class DockerParser {
     }
 
     public File getFlatDockerFile(File dockerFile) throws IOException {
-        System.out.println("**Parent path og this Dockerfile:" + dockerFile.getParentFile().getPath());
         File flatDockerfile = new File(dockerFile.getParentFile().getPath() + "\\DockerFileFlat");
+        if(!flatDockerfile.exists()){
+            flatDockerfile = this.findFile("Dockerfile",new File(this.localPath + "/" + this.localDockerfilePath) );
+        }
         BufferedWriter writer = new BufferedWriter(new FileWriter(flatDockerfile));
         FileInputStream fis = new FileInputStream(dockerFile);
 
@@ -119,11 +137,12 @@ public class DockerParser {
         return getFlatDockerFile(dockerfile);
     }
 
-    public File getFlatDockerFile() throws IOException {
+    public File getFlatDockerFile(String fileName) throws IOException {
         String localPathtoDockerfile = localPath.concat("/").concat("/").concat(localDockerfilePath);
-        File dockerfile = findFile("Dockerfile", new File(localPathtoDockerfile));
+        File dockerfile = findFile(fileName, new File(localPathtoDockerfile));
         return getFlatDockerFile(dockerfile);
     }
+
 
     private static boolean doesLineHaveAnInstruction(String line) {
         if (line.contains("ADD")) {
@@ -174,12 +193,19 @@ public class DockerParser {
     }
 
     public void fillDockerFileObject(Instruction instructions) {
-        if (instructions instanceof From) {
-            System.out.println("ITS A FROM");
-        }
     }
 
-    public Instruction getClassification(String line) {
+    public Instruction getClassification(String newLine) {
+        String line = newLine;
+        if (line.startsWith("#")) {
+            return null;
+        }
+
+        if(line.contains("\t")){
+            String uname = " ";
+            line = line.replaceAll("\t", uname);
+        }
+
         String arr[] = line.split(" ", 2);
 
         String instruction = arr[0];   //Instruction
@@ -189,6 +215,7 @@ public class DockerParser {
         } else {
             command = "";
         }
+
 
         switch (instruction) {
             case "FROM":
@@ -234,7 +261,7 @@ public class DockerParser {
     private Instruction parseExposeInstruction(String ports) {
         String[] parts = ports.split(" ");
         for (int i = 0; i < parts.length; i++) {
-            exposes.add(new Expose(parts[i]));
+            exposes.add(new Expose(dockerfile,parts[i]));
         }
         return null;
     }
@@ -271,7 +298,9 @@ public class DockerParser {
             }
 
         }
-        entryPoints.add(new EntryPoint(executable, params));
+        if(executable.length()>0){
+            entryPoint = new EntryPoint(dockerfile, executable, params);
+        }
         return null;
 
     }
@@ -280,7 +309,6 @@ public class DockerParser {
         String source = null;
         String destinatation = null;
         if (command.contains("[")) {
-            System.out.println("copyAndAdd YES IT CONTAINS [");
             Pattern p = Pattern.compile("\"(.*?)\"");
             Matcher m = p.matcher(command);
 
@@ -291,30 +319,26 @@ public class DockerParser {
             for (int i = 0; i < matches.size(); i++) {
                 if (i == 0) {
                     source = matches.get(i);
-                    System.out.println("this is exec:[ + " + source);
                 } else {
                     source = matches.get(i);
                 }
             }
         } else {
-            System.out.println("copyAndAdd No it doesnt contain[");
             String[] parts = command.split(" ");
             for (int i = 0; i < parts.length; i++) {
                 if (i == 0) {
                     source = parts[i];
-                    System.out.println("this is source:" + source);
 
                 } else {
-                    System.out.println("this is dest: " + parts[i]);
-                    source = parts[i];
+                    destinatation = parts[i];
                 }
             }
         }
 
         if (instruction.equals("ADD")) {
-            adds.add(new Add(source, destinatation));
+            adds.add(new Add(dockerfile, source, destinatation));
         } else if (instruction.equals("COPY")) {
-            copies.add(new Copy(source, destinatation));
+            copies.add(new Copy(dockerfile, source, destinatation));
         }
         return null;
     }
@@ -329,8 +353,8 @@ public class DockerParser {
     }
 
     private Instruction parseWorkDirInstruction(String path) {
-        workDirs.add(new WorkDir(path));
-        return new WorkDir(path);
+        workDirs.add(new WorkDir(dockerfile, path));
+        return new WorkDir(dockerfile, path);
     }
 
     private Instruction parseLabelInstruction(String command) {
@@ -347,19 +371,18 @@ public class DockerParser {
                 value = m.group(1);
             }
 
-            labels.add(new Label(key, value));
-            System.out.println("Label key: " + key + " and value: " + value);
+            labels.add(new Label(dockerfile, key, value));
 
         }
-        return new Label("", "");
+        return new Label(dockerfile, "", "");
     }
 
     private Instruction parseHealthCheckInstruction(String command) {
         if (command.equals("NONE")) {
         } else {
-            healthchecks.add(new Healthcheck(getClassification(command)));
+            healthcheck = new Healthcheck(dockerfile, getClassification(command));
         }
-        return new Healthcheck(getClassification(command));
+        return new Healthcheck(dockerfile, getClassification(command));
     }
 
     private Instruction parseCMDInstruction(String command) {
@@ -394,13 +417,18 @@ public class DockerParser {
             }
 
         }
-        cmds.add(new Cmd(executable, params));
+        // cmds.add(new Cmd(dockerfile,executable, params));
+        if(executable.length()>0){
+            cmd = new Cmd(dockerfile, executable, params);
+        }
         return null;
     }
 
     private Instruction parseStopSignalInstruction(String signal) {
-        stopSignals.add(new StopSignal(signal));
-        return new StopSignal(signal);
+        //stopSignals.add(new StopSignal(dockerfile,signal));
+        stopSignal = new StopSignal(dockerfile, signal);
+
+        return new StopSignal(dockerfile, signal);
 
 
     }
@@ -411,11 +439,19 @@ public class DockerParser {
 
     }
 
-    private Instruction parseRunInstruction(String command) {
+    private Instruction parseRunInstruction(String commandx) {
+        String command = commandx;
+        if(commandx.contains("(")){
+            Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(commandx);
+            while(m.find()) {
+                command  =m.group(1);
+            }
+        }
+
         String[] runentry = command.split(" && ");
 
         for (String run : runentry) {
-            String executable = null;
+            String executable = "";
             List<String> params = new ArrayList<>();
 
             if (command.contains("[")) {
@@ -434,7 +470,7 @@ public class DockerParser {
                         params.add(matches.get(i));
                     }
                 }
-            } else {
+            }else {
                 String[] parts = run.split(" ");
 
                 for (int i = 0; i < parts.length; i++) {
@@ -447,8 +483,9 @@ public class DockerParser {
                 }
 
             }
-            runs.add(new Run(executable,params));
-
+            if(executable.length()>0){
+                runs.add(new Run(dockerfile, executable, params));
+            }
         }
         return null;
     }
@@ -456,7 +493,6 @@ public class DockerParser {
 
     private Instruction parseVolumeInstruction(String command) {
         if (command.contains("[")) {
-            System.out.print("YES IT CONTAINS [");
             Pattern p = Pattern.compile("\"(.*?)\"");
             Matcher m = p.matcher(command);
 
@@ -467,28 +503,25 @@ public class DockerParser {
 
             for (String match :
                     matches) {
-                volumes.add(new Volume(match));
+                volumes.add(new Volume(dockerfile, match));
             }
         } else {
-            System.out.println("No it doesnt contain[");
             String[] parts = command.split(" ");
             for (int i = 0; i < parts.length; i++) {
-                System.out.println("No it doesnt contain[ + " + parts[i]);
-                volumes.add(new Volume(parts[i]));
+                volumes.add(new Volume(dockerfile, parts[i]));
             }
         }
-
-        return new Volume("test");
+        return new Volume(dockerfile, "test");
     }
 
     private Instruction parseOnBuildInstruction(String command) {
-        onBuilds.add(new OnBuild(getClassification(command)));
-        return new OnBuild(getClassification(command));
+        onBuilds.add(new OnBuild(dockerfile, getClassification(command)));
+        return new OnBuild(dockerfile, getClassification(command));
     }
 
     private Instruction parseUserInstruction(String command) {
-        users.add(new User(command));
-        return new User(command);
+        users.add(new User(dockerfile, command));
+        return new User(dockerfile, command);
 
 
     }
@@ -498,39 +531,49 @@ public class DockerParser {
     }
 
     private Instruction parseMaintainerInstruction(String user) {
-        maintainers.add(new Maintainer(user));
-        return new Maintainer(user);
+        maintainer = new Maintainer(dockerfile, user);
+        return new Maintainer(dockerfile, user);
 
 
     }
 
-    private Instruction parseEnvInstruction(String command) {
+    private Env parseEnvInstruction(String command) {
         String[] parts = command.split(" ");
         String key = parts[0];
         String value = "";
         if (parts.length > 1) {
             value = parts[1];
         }
-        envs.add(new Env(key, value));
-        return new Env(key, value);
+        envs.add(new Env(dockerfile, key, value));
+        return new Env(dockerfile, key, value);
     }
 
     private Instruction parseFromInstruction(String command) {
-        From from = null;
         if (command.contains("/")) {
-            from = new From(command);
+            from = new From(dockerfile,command);
+            return from;
         } else if (command.contains("@")) {
             String[] parts = command.split("@");
-            from = new From(command, parts[0], "digest");
+            String image = parts[0];
+            String imageVersion = parts[1];
+            from = new From(dockerfile,image, imageVersion, "digest");
+            return from;
         } else if (command.split("\\w+").length == 0) {
-            from = new From(command);
+            from = new From(dockerfile,command);
+            return from;
         } else if (command.matches(".*\\d+.*")) {
             Double imageVersion = Double.parseDouble(getStringFromRegexPattern("\\d+\\.?\\d+", command));
             String[] parts = command.split(":");
             String image = parts[0];
-            from = new From(image, imageVersion);
+            from = new From(dockerfile,image, imageVersion);
+            return from;
+        } else if (command.matches("\\w*(:)\\w+")){
+            String[] parts = command.split(":");
+            String image = parts[0];
+            String imageVersion = parts[1];
+            from = new From(dockerfile,image, imageVersion, "imageVersion");
+            return from;
         }
-        froms.add(from);
         return from;
     }
 
@@ -552,10 +595,15 @@ public class DockerParser {
     public String getStringFromRegexPattern(String patternInput, String data) {
         Pattern pattern = Pattern.compile(patternInput);
         Matcher matcher = pattern.matcher(data);
-        if (matcher.find()) {
-            System.out.println("getStringFromRegexPattern" + matcher.group(0));
-        }
-        return matcher.group(0);
+       try {
+           if (matcher.find()) {
+             //  System.out.println("getStringFromRegexPattern" + matcher.group(0));
+           }
+           return matcher.group(0);
+       }catch (Exception e){
+           e.printStackTrace();
+           return "";
+       }
     }
 
     public File findFile(String name, File path) {
@@ -572,33 +620,35 @@ public class DockerParser {
     }
 
     public void assignToDockerObject() {
-        dockerfile.froms = this.froms;
-        dockerfile.maintainers = this.maintainers;
+        dockerfile.from = from;
+        dockerfile.maintainer = maintainer;
+        dockerfile.cmd = cmd;
+        dockerfile.entryPoint = entryPoint;
+        dockerfile.stopSignals = stopSignal;
+        dockerfile.healthCheck = healthcheck;
+
+
         dockerfile.runs = this.runs;
-        dockerfile.cmds = this.cmds;
         dockerfile.labels = this.labels;
         dockerfile.envs = this.envs;
         dockerfile.exposes = this.exposes;
         dockerfile.adds = this.adds;
         dockerfile.copies = this.copies;
-        dockerfile.entryPoints = this.entryPoints;
         dockerfile.volumes = this.volumes;
         dockerfile.users = this.users;
         dockerfile.workDirs = this.workDirs;
         dockerfile.args = this.args;
         dockerfile.onBuilds = this.onBuilds;
-        dockerfile.stopSignals = this.stopSignals;
-        dockerfile.healthchecks = this.healthchecks;
     }
 
-    @Override
+   /* @Override
     public String toString() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(JsonMethod.FIELD, JsonAutoDetect.Visibility.ANY);
         String json = null;
         try {
             //Convert object to JSON string and save into file directly
-            mapper.writeValue(new File("\\dockerfile.json"), dockerfile);
+            mapper.writeValue(new File(localPath + "/" + "dockerfile.json"), dockerfile);
 
             //Convert object to JSON string
             json = mapper.writeValueAsString(dockerfile);
@@ -621,4 +671,9 @@ public class DockerParser {
         String footer = "*************************************************** \n";
         return header + prettyJsonString + footer;
     }
+
+    public void printDockerfile(DockerfileSnapshot dockerfileSnapshot) {
+        System.out.println(this.toString());
+
+    }*/
 }
