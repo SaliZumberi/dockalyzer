6392aff637262c7cecdf2b4a3cc98bdd92e5ddfc
Sali Zumberi <sali.zumberi@live.de>
2016-12-15 11:00:42 +0100

    add changed files and types of diffs

---
+++ src/main/java/dockalyzer/models/diff/enums/DelType.java
@@ -0,0 +1,42 @@
+package dockalyzer.models.diff.enums;
+
+/**
+ * Created by salizumberi-laptop on 03.12.2016.
+ */
+public enum DelType {
+    ADD,
+    CMD,
+    COPY,
+    ENTRYPOINT,
+    ENV,
+    EXPOSE,
+    EXECUTABLE_PARAMETER,
+    FROM,
+    LABEL,
+    MAINAINER,
+    ONBUILD,
+    RUN,
+    STOPSIGNAL,
+    USER,
+    VOLUME,
+    WORKDIR,
+    SOURCE,
+    DESTINATION,
+    ARG,
+    EXECUTABLE,
+    PARAMETER,
+    KEY,
+    VALUE,
+    PORT,
+    IMAGE,
+    IMAGE_NAME,
+    IMAGE_VERSION_STRING,
+    IMAGE_VERSION_NUMBER,
+    IMAGE_VERSION_DIGEST,
+    OPTION_PARAMETER,
+    MAINTAINER,
+    SIGNAL,
+    USER_NAME,
+    PATH,
+    HEALTHCHECK
+}
